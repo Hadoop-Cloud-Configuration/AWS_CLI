77e7c55b0464f8177078a400dc482deb545657d3
Jesse Xu <jesse.xu.20@gmail.com>
2015-09-27 16:46:57 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ curl -sSL https://get.docker.com/ | sh
 sudo usermod -aG docker ubuntu
 newgrp docker
 docker pull sequenceiq/ambari:1.7.0
-curl -Lo .amb j.mp/docker-ambari-170 && . .amb
-amb-deploy-cluster 3
+# curl -Lo .amb j.mp/docker-ambari-170 && . .amb
+docker run -d --dns 127.0.0.1 -p 8080:8080 --entrypoint /usr/local/serf/bin/start-serf-agent.sh -e KEYCHAIN= --name amb0 -h amb0.mycorp.kom sequenceiq/ambari:1.7.0 --tag ambari-server=true 
