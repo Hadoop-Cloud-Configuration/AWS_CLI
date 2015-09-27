b1f00d8503cce844347342c601156346522b0c9e
Jesse Xu <jesse.xu.20@gmail.com>
2015-09-27 16:30:13 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 curl -sSL https://get.docker.com/ | sh
-sudo docker pull sequenceiq/ambari:1.7.0
 sudo usermod -aG docker ubuntu
 newgrp docker
+docker pull sequenceiq/ambari:1.7.0
 curl -Lo .amb j.mp/docker-ambari-170 && . .amb
 amb-deploy-cluster 3
