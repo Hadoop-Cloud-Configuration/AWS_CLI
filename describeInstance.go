d5c9f56aecc3679bfe6f436d4ff62bd050af71aa
Jesse <jesse.xu.20@gmail.com>
2015-11-25 21:24:29 -0500

    finish describe instance binary

--- describeInstance.go
+++ describeInstance.go
@@ -1,30 +1,41 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
 func main() {
-    // Create an EC2 service object in the "us-west-2" region
-    // Note that you can also configure your region globally by
-    // exporting the AWS_REGION environment variable
-    svc := ec2.New(&aws.Config{Region: aws.String("us-east-1")})
+	// Create an EC2 service object in the "us-west-2" region
+	// Note that you can also configure your region globally by
+	// exporting the AWS_REGION environment variable
+	svc := ec2.New(&aws.Config{Region: aws.String("us-east-1")})
 
-    // Call the DescribeInstances Operation
-    resp, err := svc.DescribeInstances(nil)
-    if err != nil {
-        panic(err)
-    }
+	// Call the DescribeInstances Operation
+	resp, err := svc.DescribeInstances(nil)
+	if err != nil {
+		panic(err)
+	}
 
-    // resp has all of the response data, pull out instance IDs:
-    fmt.Println("> Number of reservation sets: ", len(resp.Reservations))
-    for idx, res := range resp.Reservations {
-        fmt.Println("  > Number of instances: ", len(res.Instances))
-        for _, inst := range resp.Reservations[idx].Instances {
-            fmt.Println("    - Instance ID: ", *inst.InstanceId)
-        }
-    }
+	// resp has all of the response data, pull out instance IDs:
+	fmt.Println("> Number of reservation sets: ", len(resp.Reservations))
+	var dnsName []string
+	var ips []string
+	for idx, _ := range resp.Reservations {
+		// fmt.Println("  > Number of instances: ", len(res.Instances))
+		for _, inst := range resp.Reservations[idx].Instances {
+			dnsName = append(dnsName, *inst.NetworkInterfaces[0].PrivateDnsName)
+			ips = append(ips, *inst.NetworkInterfaces[0].Association.PublicIp)
+			// fmt.Println("    - Instance ID: ", *inst.InstanceId)
+		}
+	}
+	for _, x := range dnsName {
+		fmt.Println(x)
+	}
+	for _, x := range ips {
+		fmt.Println(x)
+	}
+
 }
